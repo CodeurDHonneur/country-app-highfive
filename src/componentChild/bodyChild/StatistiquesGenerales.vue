b0e383d48f80b04ec371ac63dd7cc7fd83323bb8
CodeurDHonneur <kkodjoaugustin@gmail.com>
2024-10-04 22:21:54 +0200

    démarrage statistiques générales

--- src/componentChild/bodyChild/StatistiquesGenerales.vue
+++ src/componentChild/bodyChild/StatistiquesGenerales.vue
@@ -3,22 +3,24 @@
 
 <template>
     <div id="cap">
-        <p><span>Capitale: </span> Porto-Novo</p>
-        <p><span>Population: </span> 12123198</p>
+      <h2>Statistiques Generales </h2>
+       <p>Population mondiale : </p>
+       <p>Pays le plus peuplé : </p>
+       <p>Pays le moins peuplé : </p>
+       <p>Pays avec la plus grande superficie</p>
+       <p>Pays avec la plus petite superfice</p>
     </div>
 </template>
 
 <style scoped>
-span{
-    font-weight: bold;
-  }
+
  div{
     background-color: white;
     border-radius: 15px;
     
-    height: 130px;
+    
     width: 70%;
-    padding: 40px;
+    padding: 20px;
     margin: 20px;
   }
   #cap {
